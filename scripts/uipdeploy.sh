d56d0e112df2a83d84d1598e1d236828bb05d31c
Mariusz Buras <mariusz.buras@gmail.com>
2014-09-03 09:17:56 +0100

    start working on uipdelopy script
    a script which synchronisez entire folder from a PC to Atari.
    This is one way sync, so it's most useful for developers
    uploading code for testing.

--- scripts/uipdeploy.sh
+++ scripts/uipdeploy.sh
@@ -4,22 +4,38 @@
 local_path=$1
 host_path=$2
 
-function curl_upload_arg {
-    line=$1
-    unique=${line/*}
-    printf " -T $line --url $host_path/%s%s" ${unique##*${local_path}} "/ "    
+function curl_commandline {
+    while read line
+    do
+        unique=${line%/*}
+        printf " -T $line --url $host_path/%s%s" ${unique##*${local_path}} "/ "  
+    done
 }
 
-function find_files {
+function hash_files {
     find $local_path -type f | \
         while read line
         do
-            unique=${line%/*}
-            printf " -T $line --url $host_path/%s%s" ${unique##*${local_path}} "/ "  
+        	echo $line
         done
 }
+#echo $(hash_files)
 
-curl -v -H "Expect:" $(find_files) 2>&1 | grep PUT
+files_to_send=""
+sum_path=${host_path}/sum.md5
+sums=$(curl -s -f --url ${sum_path} | gzip -d -)
+if (($? > 0)); then
+    files_to_send=$(hash_files) 
+    md5sum $(hash_files) | gzip -c - | curl -0 -X PUT --data-binary @- ${sum_path}
+else
+	files_to_send=$(printf "$sums" | md5sum --quiet -c - 2> /dev/null | grep FAILED | sed -e 's/\(.*:\).*/\1/' -e 's/://g' )
+fi
+
+
+echo "$files_to_send" | curl_commandline
+
+
+#curl -v -H "Expect:" $(find_files) 2>&1 | grep PUT
 
 #echo $(curl_upload_arg /cygdrive/e/Trash/sndh_sf/history.txt)
 #echo $(curl_upload_arg /cygdrive/e/Trash/sndh_sf/bla/bla/history.txt)
