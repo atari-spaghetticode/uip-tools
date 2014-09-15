db32753e1bc8ceff39ef77656e91b713216ce438
Mariusz Buras <mariusz.buras@gmail.com>
2014-09-15 01:30:54 +0100

    WIP

--- scripts/uipdeploy.sh
+++ scripts/uipdeploy.sh
@@ -8,7 +8,7 @@ function curl_commandline {
     while read line
     do
         unique=${line%/*}
-        printf " -T $line --url $host_path/%s%s" ${unique##*${local_path}} "/ "  
+        printf " -T $line --url $host_path/%s%s " ${unique##*${local_path}} "/ "
     done
 }
 
@@ -20,20 +20,23 @@ function hash_files {
         done
 }
 #echo $(hash_files)
-
+local_files=$(hash_files)
 files_to_send=""
 sum_path=${host_path}/sum.md5
 sums=$(curl -s -f --url ${sum_path} | gzip -d -)
 if (($? > 0)); then
-    files_to_send=$(hash_files) 
-    md5sum $(hash_files) | gzip -c - | curl -0 -X PUT --data-binary @- ${sum_path}
+    files_to_send=$local_files 
 else
 	files_to_send=$(printf "$sums" | md5sum --quiet -c - 2> /dev/null | grep FAILED | sed -e 's/\(.*:\).*/\1/' -e 's/://g' )
 fi
 
-
-echo "$files_to_send" | curl_commandline
-
+if [ -n "$files_to_send" ]; then
+	files=$(md5sum $local_files)
+	printf "$files" | gzip -q -c - | curl -0 -X PUT --data-binary @- ${sum_path}
+	curl -s -H "Expect:" $(echo "$files_to_send" | curl_commandline) 2>&1
+else
+	echo "all up to date"
+fi
 
 #curl -v -H "Expect:" $(find_files) 2>&1 | grep PUT
 
