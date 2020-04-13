82871639ea4a1f2514d3c426b896ebc8ca060dcb
sqward <mariusz.buras@gmail.com>
2020-04-13 21:55:02 +0100

    APP: improve logging a bit
    Make the logging functions a bit more useful.

--- src/logging.h
+++ src/logging.h
@@ -3,15 +3,17 @@
 
 #include <stdio.h>
 
+#define LOG_PRINT(_fmt, ...) (printf("[%s:%d]: " _fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__))
+
 #if DEBUG==1
 	#define INFO(...) (printf(__VA_ARGS__))
-	#define LOG(...) (printf(__VA_ARGS__))
-	#define LOG_WARN(...) (printf(__VA_ARGS__))
-	#define LOG_TRACE(...) (printf(__VA_ARGS__))
+	#define LOG(_fmt, ...) LOG_PRINT(_fmt, ##__VA_ARGS__)
+	#define LOG_WARN(_fmt, ...) LOG_PRINT(_fmt, ##__VA_ARGS__)
+	#define LOG_TRACE(_fmt, ...) LOG_PRINT(_fmt, ##__VA_ARGS__)
 #else
 	#define INFO(...) (printf(__VA_ARGS__))
 	#define LOG(...)
-	#define LOG_WARN(...) (printf(__VA_ARGS__))
+	#define LOG_WARN(_fmt, ...) LOG_PRINT(_fmt, ##__VA_ARGS__)
 	#define LOG_TRACE(...)
 #endif
 
